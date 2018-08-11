e9bcbf4260048ce6a4bab3aee0df93e077d11613
Suman Kumar Hansada <sumansjs@gmail.com>
2018-08-12 00:12:26 +0530

    Expedia Angular Finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,10 +19,20 @@ export class HomeComponent implements OnInit {
   undefinedOption: any;
   hasCategoryError = true;
   finalTotal: number;
-  constructor() { }
+  taxTotal: number;
+  constructor() {
+    this.finalTotal = 0;
+    this.taxTotal = 0;
+    this.name = '';
+    this.category = '';
+    this.quantity = 0;
+    this.price = 0;
+    this.taxRate = 0;
+    this.totalPrice = 0;
+  }
 
   ngOnInit() {
-    this.finalTotal = 0;
+
     // this.invoice = [
     //   {name: 'XYZ', quantity: 5, category : 'Medical', price: 10, taxRate : null, totalPrice: null},
     //   {name: 'PQR', quantity: 7, category : 'NonMedical', price: 15, taxRate : null, totalPrice: null}
@@ -36,6 +46,8 @@ export class HomeComponent implements OnInit {
 
   removeItem(index: number) {
     this.invoice.splice(index, 1);
+    this.taxTotal = this.invoice.reduce((previousValue, currentValue) =>
+    previousValue += isNaN(currentValue.totalPrice) ? 0 : currentValue.totalPrice - (currentValue.price * currentValue.quantity), 0);
     this.finalTotal = this.invoice.reduce((previousValue, currentValue) =>
     previousValue += currentValue.totalPrice, 0);
   }
@@ -52,9 +64,12 @@ export class HomeComponent implements OnInit {
     this.invoice[row].quantity = _quantity;
     const amount = this.invoice[row].price * this.invoice[row].quantity;
     if (!isNaN(amount) && !isNaN(this.invoice[row].taxRate)) {
-      this.invoice[row].totalPrice = amount - (this.invoice[row].taxRate * amount);
+      this.invoice[row].totalPrice = amount + (this.invoice[row].taxRate * amount);
+      this.taxTotal = this.invoice.reduce((previousValue, currentValue) =>
+      previousValue += isNaN(currentValue.totalPrice) ? 0 : currentValue.totalPrice - (currentValue.price * currentValue.quantity), 0);
+
       this.finalTotal = this.invoice.reduce((previousValue, currentValue) =>
-      previousValue += currentValue.totalPrice, 0);
+      previousValue += isNaN(currentValue.totalPrice) ? 0 : currentValue.totalPrice, 0);
     }
   }
   changeCategory(row: number, _category: string) {
@@ -71,20 +86,26 @@ export class HomeComponent implements OnInit {
     }
     const amount = this.invoice[row].price * this.invoice[row].quantity;
     if (!isNaN(amount) && !isNaN(this.invoice[row].taxRate)) {
-      this.invoice[row].totalPrice = amount - (this.invoice[row].taxRate * amount);
+      this.invoice[row].totalPrice = amount + (this.invoice[row].taxRate * amount);
+      this.taxTotal = this.invoice.reduce((previousValue, currentValue) =>
+      previousValue += isNaN(currentValue.totalPrice) ? 0 : currentValue.totalPrice - (currentValue.price * currentValue.quantity), 0);
       this.finalTotal = this.invoice.reduce((previousValue, currentValue) =>
-      previousValue += currentValue.totalPrice, 0);
+      previousValue += isNaN(currentValue.totalPrice) ? 0 : currentValue.totalPrice, 0);
     } else if (isNaN(this.invoice[row].taxRate)) {
       this.invoice[row].totalPrice = NaN;
+      this.finalTotal = this.invoice.reduce((previousValue, currentValue) =>
+      previousValue += isNaN(currentValue.totalPrice) ? 0 : currentValue.totalPrice, 0);
     }
   }
   changePrice(row: number, _price: number) {
     this.invoice[row].price = _price;
     const amount = this.invoice[row].price * this.invoice[row].quantity;
     if (!isNaN(amount) && !isNaN(this.invoice[row].taxRate)) {
-      this.invoice[row].totalPrice = amount - (this.invoice[row].taxRate * amount);
+      this.invoice[row].totalPrice = amount + (this.invoice[row].taxRate * amount);
+      this.taxTotal = this.invoice.reduce((previousValue, currentValue) =>
+      previousValue += isNaN(currentValue.totalPrice) ? 0 : currentValue.totalPrice - (currentValue.price * currentValue.quantity), 0);
       this.finalTotal = this.invoice.reduce((previousValue, currentValue) =>
-      previousValue += currentValue.totalPrice, 0);
+      previousValue += isNaN(currentValue.totalPrice) ? 0 : currentValue.totalPrice, 0);
     }
   }
 }
